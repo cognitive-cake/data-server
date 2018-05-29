f12268742a7c51366adc146f0a4a98742bd17d3e
Nick-ch <nd.chernikov@gmail.com>
2018-05-29 03:47:33 +0300

    fix cors

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://cognitive-cake.github.io");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
